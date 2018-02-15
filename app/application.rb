278c361f97c430efbc870eb0e14e702b0231a801
Rauof M <abdelrauof197@gmail.com>
2018-02-15 02:12:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item = @@items.find{ |i| i.name == item_name }
       resp.write "it will cost you #{item.price}$" 
     else 
-      resp.write "page not found"
+      resp.write "Route not found"
       resp.status = 404 
     end
     
