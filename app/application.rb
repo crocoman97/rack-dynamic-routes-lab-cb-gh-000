5842b4e4bbfe3bfa1bb2efb3e0faad1c749b354e
Rauof M <abdelrauof197@gmail.com>
2018-02-15 02:21:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/")
       item = @@items.find{ |i| i.name == item_name }
-      resp.write "#{}" 
+      resp.write "mack" 
     else 
       resp.write "Route not found"
       resp.status = 404 
