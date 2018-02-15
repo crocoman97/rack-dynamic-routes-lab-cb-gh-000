d229670f80df21749a3ffa7d9e7c57660a2df2b0
Rauof M <abdelrauof197@gmail.com>
2018-02-15 02:19:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/")
       item = @@items.find{ |i| i.name == item_name }
-      resp.write "it will cost you #{item.name}$" 
+      resp.write item.name 
     else 
       resp.write "Route not found"
       resp.status = 404 
